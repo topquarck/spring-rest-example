fcd8f7abdfb7bd44c23021a8aadac15549922dfe
ibrahim.ali2 <ibrahim.ali2@ibm.com>
2019-10-14 16:47:26 +0200

    code added to handle the webex auth

--- src/main/java/com/ibm/eib/rest/RestController.java
+++ src/main/java/com/ibm/eib/rest/RestController.java
@@ -1,14 +1,31 @@
 package com.ibm.eib.rest;
 
+import java.security.Provider.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ibm.eib.service.WebexService;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/hi")
 public class RestController {
 
+	@Autowired
+	private WebexService webexService;
+	
+	public WebexService getWebexService() {
+		return webexService;
+	}
+	public void setWebexService(WebexService webexService) {
+		this.webexService = webexService;
+	}
+	 
 	@RequestMapping(method = RequestMethod.GET, value = "/sayhi")
-	public String sayHi() {
-		return " Heloooooooooooooooooooooooo!!!";
+	public String sayHi(@RequestParam("code") String code) {
+		
+		return webexService.generateWidget(code);
 	}
 }
---
+++ src/main/java/com/ibm/eib/service/WebexService.java
@@ -0,0 +1,83 @@
+package com.ibm.eib.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+
+@Service
+public class WebexService {
+
+	@Autowired
+    ResourceLoader resourceLoader;
+	
+	private String authUrl = "https://api.ciscospark.com/v1/access_token";
+	
+	public String generateWidget(String code) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		try {
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+			HttpEntity<String> request = new HttpEntity<String>(createInputString(code), headers);
+			
+			AuthJson resultObject = 
+					restTemplate.postForObject(authUrl,request, AuthJson.class);
+
+			if (resultObject.getAccess_token()!= null) {
+				return generatePage(resultObject.getAccess_token());
+			} 
+			return "<h1>access token is null!!</h1>";
+			
+		} catch (RestClientException exception) {
+			System.out.println("REST and HTTP error" + exception.getMessage());
+			throw exception;
+		}
+	}
+
+	private String generatePage(String access_token) {
+
+		Resource resource = resourceLoader.getResource("classpath:templates/widget.html");
+
+		try {
+			InputStream input = resource.getInputStream();
+			File file = resource.getFile();
+			String allString = FileUtils.readFileToString(file,StandardCharsets.UTF_8);
+			allString = allString.replace("$accessToken", access_token);
+			return allString;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "<h1>failed to open the file</h1>";
+		}
+
+		
+	}
+
+	private String createInputString(String code) {
+
+		StringBuilder builder = new StringBuilder();
+		builder.append("grant_type=authorization_code&");
+		builder.append("client_id=Cba89729a684e25751b9a8a43bc881190b518c3b533c5f2f134db61b49626d71c&");
+		builder.append("client_secret=0a72f86d4db087f17e68b5d37dd15ae2baed18cbf92fc5f5a4116c9a907983cb&");
+		builder.append("code="+code+"&");
+		builder.append("redirect_uri=http://spring-hi-trying-spring.apps.ca-central-1.starter.openshift-online.com/hi/sayhi&");
+		
+		return builder.toString();
+	}
+
+}
